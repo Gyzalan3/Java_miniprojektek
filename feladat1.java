2386cac61e356615147dab70ab9c8551d57b65cf
Zalcsy3 <zalan90909@gmail.com>
2024-02-20 18:35:25 +0200

    feladat1

--- feladat1.java
+++ feladat1.java
@@ -1,4 +1,18 @@
 package Java_miniprojektek;
 
 public class feladat1 {
+    public static void main(String[] args) {
+        int n;
+        int i;
+        n = 8;
+        for (i = 1; i <= 10; i++) {
+            if (n >= 1 && n <= 10) {
+                System.out.println(i + "*" + n + "=" + (n * i));
+            }
+            else {
+                System.out.print("Hiba történt nem jó a szám, mivel nem 1 és 10 között van!");
+                break;
+            }
+        }
+    }
 }
